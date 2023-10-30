c80abe1f40555607385c5efeac80f1ffde62d678
Jean Amaral <jeancarlosimpliamaral@hotmail.com>
2023-10-29 23:54:02 -0300

    Registration of informations in progress

--- src/http/views/src/Pages/Candidato/cadastroInformacoes.js
+++ src/http/views/src/Pages/Candidato/cadastroInformacoes.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useRef } from "react";
 import MultiStep from "react-multistep";
 import "./cadastroInformacoes.css";
@@ -12,7 +14,64 @@ export default function CadastroInfo() {
   function BasicInfoDiv() {
     return (
       <div className="fill-container">
-        <div className="input-cadastro"></div>
+        <div className="input-cadastro title">
+          <h2>1. Identificação</h2>
+        </div>
+        <div className="input-cadastro hidden-title"></div>
+        <div className="input-cadastro hidden-title"></div>
+
+        <div className="input-cadastro">
+          <input type="text" placeholder="Nome completo"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="date" placeholder=""></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Sexo"></input>
+        </div>
+
+        <div className="input-cadastro title">
+          <h2>2. Naturalidade</h2>
+        </div>
+        <div className="input-cadastro hidden-title"></div>
+        <div className="input-cadastro hidden-title"></div>
+
+        <div className="input-cadastro">
+          <input type="text" placeholder="Estado"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Cidade"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Nacionalidade"></input>
+        </div>
+
+        <div className="input-cadastro title">
+          <h2>3. Documentos</h2>
+        </div>
+        <div className="input-cadastro hidden-title"></div>
+        <div className="input-cadastro hidden-title"></div>
+
+        <div className="input-cadastro">
+          <input type="text" placeholder="CPF"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="RG"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Orgão Emissor"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="UF do orgão emissor"></input>
+        </div>
+        <div className="input-cadastro hidden-title"></div>
+        <div className="input-cadastro  hidden-title"></div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="E-mail" required></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Profissão"></input>
+        </div>
       </div>
     );
   }
@@ -28,7 +87,27 @@ export default function CadastroInfo() {
   function HousingInfoDiv() {
     return (
       <div className="fill-container">
-        <h1>3</h1>
+        <div className="input-cadastro">
+          <input type="text" placeholder="CEP"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Logradouro"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="nº"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Complemento"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Bairro"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Cidade"></input>
+        </div>
+        <div className="input-cadastro">
+          <input type="text" placeholder="Estado"></input>
+        </div>
       </div>
     );
   }
@@ -42,9 +121,174 @@ export default function CadastroInfo() {
   }
 
   function EarningInfoDiv() {
+    const [isSubscribed, setSubscribed] = useState(null);
+
+    useEffect(() => {
+      if (isSubscribed === "no") {
+        document.getElementById("nis-input").disabled = true;
+      } else {
+        document.getElementById("nis-input").disabled = false;
+      }
+    }, [isSubscribed]);
+
     return (
       <div className="fill-container">
-        <h1>5</h1>
+        <div className="input-cadastro title question">
+          <h2>5. Inscrito em programas de transferência de renda? </h2>
+        </div>
+        <div className="input-cadastro hidden-title"></div>
+        <div className="input-cadastro hidden-title"></div>
+        <div className="input-cadastro input-secondary">
+          <input
+            type="radio"
+            name="gov-program"
+            value="yes"
+            onClick={() => setSubscribed("yes")}
+          />
+          Sim
+          <input
+            type="radio"
+            name="gov-program"
+            value="no"
+            onClick={() => setSubscribed("no")}
+          />
+          Não
+        </div>
+        <div className="input-cadastro">
+          <input type="text" id="nis-input" placeholder="Se sim, informe NIS" />
+        </div>
+        <div className="input-cadastro hidden-title"></div>
+        <div className="input-cadastro hidden-title"></div>
+        <div className="input-cadastro input-dropbox">
+          <div>
+            <input
+              type="radio"
+              name="employmentType"
+              value="empregado-privado"
+            />
+            Empregado privado (CLT)
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="employmentType"
+              value="empregado-publico"
+            />
+            Empregado público
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="employmentType"
+              value="empregado-domestico"
+            />
+            Empregado doméstico
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="empregado-rural" />
+            Empregado temporário na área rural
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="employmentType"
+              value="empresario-simples"
+            />
+            Empresário (Simples Nacional)
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="empresario" />
+            Empresário
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="mei" />
+            MEI
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="autonomo" />
+            Autônomo (conta-própria)
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="aposentado" />
+            Aposentado
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="pensionista" />
+            Pensionista
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="aprendiz" />
+            Aprendiz ou estagiário
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="voluntario" />
+            Trabalhador voluntário
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="renda-aluguel" />
+            Renda de aluguéis ou arrendamento
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="estudante" />
+            Estudante
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="informal" />
+            Trabalhador informal
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="desempregado" />
+            Desempregado
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="desempregado" />
+            Desempregado
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="auxilio-doenca" />
+            Auxílio doença
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="prof-liberal" />
+            Profissional Liberal
+          </div>
+          <div>
+            <input type="radio" name="employmentType" value="ajuda-terceiros" />
+            Ajuda Financeira de Terceiros
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="employmentType"
+              value="pensao-alimenticia"
+            />
+            Pensão alimentícia
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="employmentType"
+              value="pensao-alimenticia"
+            />
+            Pensão alimentícia
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="employmentType"
+              value="previdencia-privada"
+            />
+            Previdência Privada
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="employmentType"
+              value="previdencia-privada"
+            />
+            Previdência Privada
+          </div>
+        </div>
       </div>
     );
   }
@@ -91,6 +335,9 @@ export default function CadastroInfo() {
         <MultiStep
           activeStep={0}
           className="multi-step"
+          stepCustomStyle={{
+            fontSize: 0.8 + "rem",
+          }}
           prevButton={{
             title: "<",
             style: {
