9f067b604f3aa703657000cf21c4837b211dff1d
Victordavi <victordavisb@gmail.com>
2023-11-27 21:16:52 -0300

    base url

--- views/src/services/axios.js
+++ views/src/services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4000', // BackEnd URL
+  baseURL: 'https://cadastraqui-production-fbcc.up.railway.app', // BackEnd URL
 })
