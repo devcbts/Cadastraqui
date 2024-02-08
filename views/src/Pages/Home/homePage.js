32530485ea47e05cdbd3a8e6129039a1f7090cb1
Jean Amaral <jeancarlosimpliamaral@hotmail.com>
2024-02-08 15:10:10 -0300

    Landing page front correções

--- views/src/Pages/Home/homePage.js
+++ views/src/Pages/Home/homePage.js
@@ -18,6 +18,10 @@ import featureThreePicture from "../../Assets/business.jpg";
 import computerImg from "../../Assets/top-image.png";
 import laptop from "../../Assets/laptop.png";
 import "react-bootstrap";
+import LogoWhite from "../../Assets/Prancheta 3@300x-8.png";
+import LogoBlue from "../../Assets/logoblue.png";
+import { Cross as Hamburger } from "hamburger-react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -97,12 +101,141 @@ export default function HomePage() {
     };
   }, []);
 
+  /* Functions related to navbar */
+
+  // isSticy is a function that verifies if the scroll of the page is zero or not
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    // Function to handle the resize event
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Attach the event handler
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // The empty array ensures this effect only runs once, on mount and unmount
+  useEffect(() => {
+    const handleScroll = () => {
+      // Update the isSticky state based on scroll position
+      setIsSticky(window.scrollY > 0);
+    };
+
+    // Attach the event listener to the window
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    // It is, when you go back to the top, isSticky is set as false again
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // A variable that defines if the menu is open or not
+  const [isOPen, setIsOpen] = useState(false);
+
+  /* --------------------------- */
+
+  function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+    setIsOpen();
+  }
+
   return (
     <>
-      <NavBarHome></NavBarHome>
+      <div>
+        <div className={`navbar-container ${isSticky ? "sticky" : ""}`}>
+          <div className={`navbar-inner ${isSticky ? "sticky-inner" : ""}`}>
+            <div className="logo-box">
+              <a
+                className={`logo-home secondary-logo ${
+                  !isSticky ? "typewriter" : ""
+                }`}
+              >
+                <img
+                  src={LogoBlue}
+                  className={`show-dark ${!isSticky ? "icon-dark" : ""}`}
+                ></img>
+              </a>
+
+              <a className={`logo-home ${isSticky ? "typewriter" : ""}`}>
+                <img src={LogoWhite} className="icon-logo" />
+                CADASTRAQUI
+              </a>
+            </div>
+            <div className={`nav-list ${isSticky ? "nav-dark" : ""}`}>
+              {windowWidth > 800 && (
+                <ul>
+                  <li>
+                    <a onClick={() => scrollToSection("inicio")}>Início</a>
+                  </li>
+                  <li>
+                    <a onClick={() => scrollToSection("recursos")}>Recursos</a>
+                  </li>
+                  <li>
+                    <a onClick={() => scrollToSection("sobre")}>Sobre</a>
+                  </li>
+                  <li>
+                    <a onClick={() => scrollToSection("contato")}>Contato</a>
+                  </li>
+                  <li>
+                    <Link to={"/login"}>
+                      <a
+                        className={`${
+                          !isSticky ? "dark-login" : "white-login"
+                        }`}
+                      >
+                        Login
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              )}
+              {windowWidth < 800 && (
+                <Hamburger
+                  toggled={isOPen}
+                  toggle={setIsOpen}
+                  color={`${isSticky ? "#1b4f73" : "#fff"}`}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className={`backmenu ${isOPen ? "backmenu-show" : ""}`}>
+          <div className="nav-buttons">
+            <ul>
+              <li>
+                <a onClick={() => scrollToSection("inicio")}>Início</a>
+              </li>
+              <li>
+                <a onClick={() => scrollToSection("recursos")}>Recursos</a>
+              </li>
+              <li>
+                <a onClick={() => scrollToSection("sobre")}>Sobre</a>
+              </li>
+              <li>
+                <a onClick={() => scrollToSection("contato")}>Contato</a>
+              </li>
+              <li>
+                <Link to={"/login"}>
+                  <a className={`${!isSticky ? "dark-login" : "white-login"}`}>
+                    Login
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div className="container-landing">
         <video className="video-header" src={videoBg} autoPlay loop muted />
-        <div className="header-box">
+        <div className="header-box" id="inicio">
           <div className="text-section">
             <h1>
               Segurança e agilidade no processo de concessão e manutenção de
@@ -117,7 +250,7 @@ export default function HomePage() {
         </div>
       </div>
       <div className="body-landing">
-        <div className="our-team">
+        <div className="our-team" id="recursos">
           <div className="team-block">
             <h1 className="great-title">
               <span>Resolvendo</span> <br></br>O Problema
@@ -126,7 +259,8 @@ export default function HomePage() {
           <div className="team-block">
             <h2>
               <h1 className="subtitle">Segurança por Lei</h1>
-              Toda a plataforma é amparada na LC n° 187/21 e Decreto nº 11.791/23 que garante segurança e embasamento jurídico.
+              Toda a plataforma é amparada na LC n° 187/21 e Decreto nº
+              11.791/23 que garante segurança e embasamento jurídico.
             </h2>
           </div>
           <div className="team-block">
@@ -148,9 +282,11 @@ export default function HomePage() {
               </div>
               <div className="team-block">
                 <h2>
-                  <h1 className="subtitle">Assistentes sociais</h1>A plataforma disponibiliza uma pré-avaliação dos candidatos,
-                   fornecendo um ranking de acordo com critérios pré-estabelecidos.
-                   O sistema possibilita a solicitação de novos documentos e agendamento de entrevistas e visitas domiciliares.
+                  <h1 className="subtitle">Assistentes sociais</h1>A plataforma
+                  disponibiliza uma pré-avaliação dos candidatos, fornecendo um
+                  ranking de acordo com critérios pré-estabelecidos. O sistema
+                  possibilita a solicitação de novos documentos e agendamento de
+                  entrevistas e visitas domiciliares.
                 </h2>
               </div>
             </>
@@ -160,9 +296,11 @@ export default function HomePage() {
             <>
               <div className="team-block">
                 <h2>
-                  <h1 className="subtitle">Assistentes sociais</h1>A plataforma disponibiliza uma pré-avaliação dos candidatos,
-                   fornecendo um ranking de acordo com critérios pré-estabelecidos.
-                   O sistema possibilita a solicitação de novos documentos e agendamento de entrevistas e visitas domiciliares.
+                  <h1 className="subtitle">Assistentes sociais</h1>A plataforma
+                  disponibiliza uma pré-avaliação dos candidatos, fornecendo um
+                  ranking de acordo com critérios pré-estabelecidos. O sistema
+                  possibilita a solicitação de novos documentos e agendamento de
+                  entrevistas e visitas domiciliares.
                 </h2>
               </div>
               <div className="team-block">
@@ -184,7 +322,7 @@ export default function HomePage() {
           </div>
         </div>
 
-        <div className="features-description">
+        <div className="features-description" id="sobre">
           <div className="feature-card">
             <img src={featureOnePicture}></img>
 
@@ -192,8 +330,8 @@ export default function HomePage() {
               <h1 className="subtitle">
                 Segurança &<br></br> Conformidade{" "}
               </h1>
-              Toda a plataforma é amparada na LC n° 187/21 e Decreto nº 11.791/23 que garante segurança e embasamento jurídico.
-
+              Toda a plataforma é amparada na LC n° 187/21 e Decreto nº
+              11.791/23 que garante segurança e embasamento jurídico.
             </h2>
           </div>
           <div className="feature-card">
@@ -235,7 +373,7 @@ export default function HomePage() {
           </div>
           </div>*/}
 
-        <div className="footer-container">
+        <div className="footer-container" id="contato">
           <div className="footer-box">
             <h2>
               <h1>Sobre</h1>
