08adcf0d4d79ce4666a38baa78e2fef265755b9a
Gabriellocp <gabriel_campista@hotmail.com>
2024-04-02 23:21:51 -0300

    chore: Add cross-env and environment scripts for dev and production

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,7 @@
+import dotenv from 'dotenv'
 import 'dotenv/config'
+import path from 'path'
 import { z } from 'zod'
-
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
   PORT: z.coerce.number().default(3333),
@@ -10,10 +11,9 @@ const envSchema = z.object({
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_KEY_ID: z.string(),
 })
-
 // Validação das variáveis de ambiente
-const _env = envSchema.safeParse(process.env)
-
+const _env = envSchema.safeParse(dotenv.config({ path: path.resolve(__dirname, '../../', `.env.${process.env.NODE_ENV}`) }).parsed)
+console.log(_env)
 if (_env.success === false) {
   console.error('❌ Invalid environment variables', _env.error.format())
 
