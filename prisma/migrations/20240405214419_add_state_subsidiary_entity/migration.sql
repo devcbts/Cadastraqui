41e03d153bbcb2bdf89ede9bdc3c62c29728e297
devcbts <devcbts01@gmail.com>
2024-04-05 18:44:50 -0300

    feat: Add UF field on Entity and Subsidiary

---
+++ prisma/migrations/20240405214419_add_state_subsidiary_entity/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "EntitySubsidiary" ADD COLUMN     "UF" "COUNTRY";
+
+-- AlterTable
+ALTER TABLE "entities" ADD COLUMN     "UF" "COUNTRY";
---
+++ src/utils/enums/zod/state.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+const STATES = z.enum([
+    'AC',
+    'AL',
+    'AM',
+    'AP',
+    'BA',
+    'CE',
+    'DF',
+    'ES',
+    'GO',
+    'MA',
+    'MG',
+    'MS',
+    'MT',
+    'PA',
+    'PB',
+    'PE',
+    'PI',
+    'PR',
+    'RJ',
+    'RN',
+    'RO',
+    'RR',
+    'RS',
+    'SC',
+    'SE',
+    'SP',
+    'TO',
+])
+
+export default STATES
