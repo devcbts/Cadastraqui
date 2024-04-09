9d4533c5b512ba314b3bbac65a5871ad60548be5
devcbts <devcbts01@gmail.com>
2024-04-09 19:27:18 -0300

    feat: Add services for user, candidate, responsible and entity

--- src/services/user/userService.js
+++ src/services/user/userService.js
@@ -2,11 +2,15 @@ import { api } from "../axios"
 
 class UserService {
     async changePassword({ oldPass, newPass }) {
+        const token = localStorage.getItem("token")
+
         await api.put('/change_password', {
             oldPassword: oldPass,
             newPassword: newPass
-        })
+        }, { headers: { Authorization: `Bearer ${token}` } })
     }
+
+
 }
 
 export default new UserService()
