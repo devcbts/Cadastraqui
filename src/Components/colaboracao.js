a01eb891bab9ed3615bde9b1e4693751a085cc9a
Guilhermesf27 <d2020011524@unifei.edu.br>
2024-04-10 23:46:09 -0300

    first-fixes

--- src/Components/colaboracao.js
+++ src/Components/colaboracao.js
@@ -51,6 +51,46 @@ export default function Colaboracao(props) {
     });
   }
 
+  const handleEditResp = () => {
+    MySwal.fire({
+      title: 'Editar Informações',
+      html: (
+        <div>
+          <h2>Nome</h2>
+          <input id="swal-input-name" className="swal2-input" defaultValue={props.name} placeholder="Nome" />
+          {/* <h2>CEP</h2>
+          <input id="swal-input-cep" className="swal2-input" defaultValue={props.CEP} placeholder="CEP" />
+          <h2>Razão Social</h2>
+          <input id="swal-input-socialReason" className="swal2-input" defaultValue={props.name} placeholder="Razão Social" />
+          <h2>Códico e-Mec/Educasenso</h2>
+
+          <input id="swal-input-educationalInstitutionCode" className="swal2-input" defaultValue={props.educationalInstitutionCode} placeholder="Código Instituição Educacional" /> */}
+        </div>
+      ),
+      focusConfirm: false,
+      preConfirm: () => {
+        const name = document.getElementById('swal-input-name').value;
+
+        // Aqui você pode adicionar mais campos conforme necessário
+        const updatedInfo = { name };
+        // Chamar função de atualização aqui
+        const token = localStorage.getItem('token');
+        api.patch(`entities/responsible/${props.id}`,  updatedInfo , {
+          headers: {
+            'authorization': `Bearer ${token}`,
+          }
+        })
+          .then(response => {
+            MySwal.fire('Atualizado!', 'As informações foram atualizadas com sucesso.', 'success');
+          })
+          .catch(error => {
+            MySwal.fire('Erro!', 'Houve um erro ao atualizar as informações.', 'error');
+            console.log(error)
+          });
+      }
+    });
+  }
+
   const handleDelete = () => {
     MySwal.fire({
       title: 'Tem certeza?',
@@ -62,35 +102,35 @@ export default function Colaboracao(props) {
       confirmButtonText: 'Sim, delete isso!'
     }).then((result) => {
       if (result.isConfirmed) {
-        // Chamar função de delete aqui
-        console.log('Deleted:', props.id);
-        // Exemplo de delete com axios
         const token = localStorage.getItem('token');
-        let type = ''
+        let type = '';
         if (props.role === 'Filial') {
-          type = 'subsidiary'
+          type = 'subsidiary';
+        } else if (props.role === 'Assistente') {
+          type = 'assistant';
+        } else if (props.role === 'Responsável') {
+          type= 'director';
         }
-        if (props.role === 'Assistente') {
-          type = 'assistant'
-        }
-        if (props.role === 'Responsável') {
-          type= 'director'
-        }
+
         api.delete(`entities/${type}/${props.id}`, {
           headers: {
             'authorization': `Bearer ${token}`,
           }
         })
-          .then(response => {
-            MySwal.fire('Deletado!', `A ${props.role} foi deletada.`, 'success');
-          })
-          .catch(error => {
-            MySwal.fire('Erro!', `Houve um erro ao deletar a ${props.role}.`, 'error');
-            console.log(error)
-          });
+        .then(response => {
+          MySwal.fire('Deletado!', `A ${props.role} foi deletada.`, 'success');
+          // Usar a função de remoção passada via props
+          if (props.onRemoveEntity) {
+            props.onRemoveEntity();
+          }
+        })
+        .catch(error => {
+          MySwal.fire('Erro!', `Houve um erro ao deletar a ${props.role}.`, 'error');
+          console.log(error);
+        });
       }
-    })
-  }
+    });
+  };
 
   return (
     <div className="card-candidatura">
@@ -107,6 +147,9 @@ export default function Colaboracao(props) {
       </div>
       {props.role === 'Filial' &&
         <button onClick={handleEdit} className="button-edital-editar" style={{marginBottom: '0px'}}>Editar</button>
+      }
+      {props.role === 'Responsável' &&
+        <button onClick={handleEditResp} className="button-edital-editar" style={{marginBottom: '0px'}}>Editar</button>
       }
       <button onClick={handleDelete} className="button-edital-excluir">Excluir</button>
     </div>
