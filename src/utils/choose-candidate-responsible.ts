7019e7d810a2b9b3c6f50b5b97f888e39ca21705
Victordavi <victordavisb@gmail.com>
2024-04-29 14:59:50 -0300

    feat: added select-candidate-responsible

--- src/utils/choose-candidate-responsible.ts
+++ src/utils/choose-candidate-responsible.ts
@@ -1,5 +1,7 @@
 import { prisma } from "@/lib/prisma";
 
+
+// This function is similar to SelectCandidateResponsible, but it diferentiates the candidate (user) from the legal dependent (non-user)
 export async function ChooseCandidateResponsible(id: string): Promise<{ IsResponsible: boolean, UserData: any } | null> {
 
     const candidate = await prisma.candidate.findUnique({
