3d5cf47ba42837e1eafe77307a0807e8522b30b6
Victordavi <victordavisb@gmail.com>
2024-05-20 17:33:50 -0300

    feat: created find-all functions to replicate data to historyDatabase

---
+++ src/HistDatabaseFunctions/Handle Application/find-all-monthly-income.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma";
+
+export default async function findAllMonthlyIncome(candidate_id: string | null, legalResponsibleId: string | null) {
+    let candidateOrResponsible
+    if (candidate_id) {
+        candidateOrResponsible = await prisma.candidate.findUnique({
+            where: { id: candidate_id },
+            include: { FamillyMember: true }
+        })
+    }
+    if (legalResponsibleId) {
+        candidateOrResponsible = await prisma.legalResponsible.findUnique({
+            where: { id: legalResponsibleId },
+            include: { FamillyMember: true }
+        })
+    }
+    if (!candidateOrResponsible) {
+        return null
+    }
+
+    const monthlyIncome = await prisma.monthlyIncome.findMany({
+        where: {
+            OR: [
+                { candidate_id: candidateOrResponsible.id },
+                { legalResponsibleId: candidateOrResponsible.id },
+                { familyMember_id: { in: candidateOrResponsible.FamillyMember.map(member => member.id) } }
+            ]
+        }
+    });
+
+    return monthlyIncome;
+}
