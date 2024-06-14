c39cafaf381710644b6bf023e3e0ed23f16e7bba
Victordavi <victordavisb@gmail.com>
2024-06-14 13:50:09 -0300

    feat: added new routes to assistant

--- src/http/controllers/candidates/get-health-info.ts
+++ src/http/controllers/candidates/get-health-info.ts
@@ -3,6 +3,7 @@ import { prisma } from '@/lib/prisma'
 import { SelectCandidateResponsible } from '@/utils/select-candidate-responsible'
 import { FamilyMember } from '@prisma/client'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { getSectionDocumentsPDF } from './AWS Routes/get-pdf-documents-by-section'
 
 
 
@@ -41,15 +42,22 @@ export async function getHealthInfo(
             include: { medications: true }
           })
 
-          const healthInfo: { disease?: any, medication: any[] }[] = familyMemberHealthInfo.map(disease => ({
+          const healthInfo: { id: string, disease?: any, specificDisease?: any, hasMedicalReport: boolean, medication: any[] }[] = familyMemberHealthInfo.map(disease => ({
+            id: disease.id,
             disease: disease,
+            hasMedicalReport: disease.hasMedicalReport,
+            specificDisease: disease.specificDisease,
             medication: disease.medications.map(medication => medication.medicationName)
           }))
           const familyMedicationInfo = await prisma.medication.findMany({
-            where: { familyMember_id: familyMember.id }
+            where: { familyMember_id: familyMember.id },
+            include: { FamilyMemberDisease: true }
           })
           familyMedicationInfo.forEach((med) => {
-            healthInfo.push({ disease: null, medication: [med] })
+            if (!med.FamilyMemberDisease) {
+
+              healthInfo.push({ id: med.id, disease: null, medication: [med], hasMedicalReport: false })
+            }
           })
           healthInfoResults.push({ name: familyMember.fullName, id: familyMember.id, healthInfo })
         } catch (error) {
@@ -63,25 +71,49 @@ export async function getHealthInfo(
       include: { medications: true }
     })
     const candidateMedications = await prisma.medication.findMany({
-      where: idField
+      where: idField,
+      include: { FamilyMemberDisease: true }
     })
-    const healthInfo: { disease?: any, medication: any[] }[] = candidateDisease.map(disease => ({
+    const healthInfo: { id: string, disease?: any, specificDisease?: any, hasMedicalReport: boolean, medication: any[] }[] = candidateDisease.map(disease => ({
+      id: disease.id,
       disease: disease,
+      hasMedicalReport: disease.hasMedicalReport,
+      specificDisease: disease.specificDisease,
       medication: disease.medications.map(medication => medication.medicationName)
     }))
 
     // Need to get MEDICATIONS that DO NOT HAVE any disease registered :)
     candidateMedications.forEach((med) => {
-      healthInfo.push({ disease: null, medication: [med] })
+      if (!med.FamilyMemberDisease) {
+        healthInfo.push({ id: med.id, disease: null, medication: [med], hasMedicalReport: false })
+      }
+
     })
 
     let healthInfoResults = await fetchData(familyMembers)
 
 
     healthInfoResults.push({ name: candidateOrResponsible.UserData.name, id: candidateOrResponsible.UserData.id, healthInfo })
+    const urlsHealth = await getSectionDocumentsPDF(candidateOrResponsible.UserData.id, 'health')
+    const urlsMedication = await getSectionDocumentsPDF(candidateOrResponsible.UserData.id, 'medication')
+    const healthInfoResultsWithUrls = healthInfoResults.map((member) => {
+      const healthInfoResultsUrls = member.healthInfo.map((disease) => {
+
+        const healthDocuments = Object.entries(urlsHealth).filter(([url]) => url.split("/")[4] === disease.id)
+        const medicationDocuments = Object.entries(urlsMedication).filter(([url]) => url.split("/")[4] === disease.id )
+        return {
+          ...disease,
+          urlsHealth: Object.fromEntries(healthDocuments),
+          urlsmedication : Object.fromEntries(medicationDocuments)
+        }
+      })
+      return {
+        healthInfoResultsUrls
+      }
+    })
 
 
-    return reply.status(200).send({ healthInfoResults })
+    return reply.status(200).send({ healthInfoResultsWithUrls })
   } catch (err: any) {
     if (err instanceof ResourceNotFoundError) {
       return reply.status(404).send({ message: err.message })
