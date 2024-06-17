bdca5f2fa1eb489e5d4e5faf788f51ef9d2b8ffb
Victordavi <victordavisb@gmail.com>
2024-06-16 21:38:11 -0300

    feat: added bankAccount find function

---
+++ src/HistDatabaseFunctions/Handle Application/find-all-bank-account.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@/lib/prisma";
+
+export default async function findAllBankAccount(candidate_id: string | null, legalResponsibleId: string | null) {
+    let candidateOrResponsible
+    if (candidate_id) {
+        candidateOrResponsible = await prisma.candidate.findUnique({
+            where: { id: candidate_id },
+            include: { FamillyMember: true }
+        })
+    }
+    if (legalResponsibleId) {
+        candidateOrResponsible = await prisma.legalResponsible.findUnique({
+            where: { id: legalResponsibleId },
+            include: { FamillyMember: true }
+        })
+    }
+    if (!candidateOrResponsible) {
+        return null
+    }
+
+
+    const income = await prisma.bankAccount.findMany({
+        where: {
+            OR: [
+                { candidate_id: candidateOrResponsible.id },
+                { legalResponsibleId: candidateOrResponsible.id },
+                { familyMember_id: { in: candidateOrResponsible.FamillyMember.map(member => member.id) } }
+            ]
+        }
+    });
+
+    return income;
+}
