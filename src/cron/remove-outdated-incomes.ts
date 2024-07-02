9ad7bca4d0d022b80dcb6868add4f6ce0e8ab86b
Victordavi <victordavisb@gmail.com>
2024-07-01 23:59:27 -0300

    fix: fixed remove outdate-incomes

--- src/cron/remove-outdated-incomes.ts
+++ src/cron/remove-outdated-incomes.ts
@@ -1,32 +1,47 @@
 import nodeSchedule from 'node-schedule'
 import { prisma } from '../lib/prisma'
 
-async function removeOutdatedIncome() {
+export async function removeOutdatedIncome() {
+    const deletedIncomes = []; // Passo 1
     try {
-        const incomes = await prisma.monthlyIncome.findMany({
+        const uniqueGroups = await prisma.monthlyIncome.findMany({
             select: {
-                id: true,
-                incomeSource: true,
-                date: true,
+                familyMember_id: true,
                 candidate_id: true,
-                familyMember_id: true
+                incomeSource: true,
             },
-            orderBy: {
-                date: 'asc'
-            },
-            distinct: ['familyMember_id', 'candidate_id', 'incomeSource']
-        })
-        await prisma.monthlyIncome.deleteMany({
-            where: {
-                id: { in: incomes.map(e => e.id) }
-            }
-        })
-    } catch (err) {
+            distinct: ['familyMember_id', 'candidate_id','legalResponsibleId', 'incomeSource']
+        });
 
+        for (const group of uniqueGroups) {
+            const oldestIncome = await prisma.monthlyIncome.findFirst({
+                where: {
+                    familyMember_id: group.familyMember_id,
+                    candidate_id: group.candidate_id,
+                    incomeSource: group.incomeSource,
+                },
+                orderBy: {
+                    date: 'asc'
+                },
+            });
+
+            if (oldestIncome) {
+                const deleted = await prisma.monthlyIncome.delete({
+                    where: {
+                        id: oldestIncome.id
+                    }
+                });
+                deletedIncomes.push(deleted); // Passo 2
+            }
+        }
+    } catch (err) {
+        // Handle error
     }
+    return deletedIncomes; // Passo 3
 }
 
-// Run this job every day 1 of the month
-const job: nodeSchedule.Job = nodeSchedule.scheduleJob("* * * 1 * * *", async () => await removeOutdatedIncome())
+const job: nodeSchedule.Job = nodeSchedule.scheduleJob("* * * 1 * * *", async () => {
+    const deletedIncomes = await removeOutdatedIncome();
+})
 
 export default job
