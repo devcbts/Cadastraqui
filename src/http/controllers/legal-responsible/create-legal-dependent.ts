da28aa3cd03043c83f3d76d70ea897835f39d9e6
devcbts <devcbts01@gmail.com>
2024-07-02 21:33:35 -0300

    fix: Removed non-existant fields from legal dependent route

--- src/http/controllers/legal-responsible/create-legal-dependent.ts
+++ src/http/controllers/legal-responsible/create-legal-dependent.ts
@@ -1,12 +1,11 @@
 import { NotAllowedError } from '@/errors/not-allowed-error'
-import { ResourceNotFoundError } from '@/errors/resource-not-found-error'
 import { UserAlreadyExistsError } from '@/errors/users-already-exists-error'
 import { prisma } from '@/lib/prisma'
 
 export async function createLegalDependent(
   name: string, CPF: string, birthDate: string, responsible_id: string
 ) {
- 
+
   try {
     const responsible = await prisma.legalResponsible.findUnique({
       where: { id: responsible_id },
@@ -25,16 +24,10 @@ export async function createLegalDependent(
 
     await prisma.candidate.create({
       data: {
-        address: responsible.address,
-        CEP: responsible.CEP,
-        city: responsible.city,
         CPF,
         birthDate: new Date(birthDate),
         name,
-        neighborhood: responsible.neighborhood,
-        addressNumber: responsible.addressNumber,
-        phone: responsible.phone,
-        UF: responsible.UF,
+
         responsible_id: responsible.id,
       },
     })
