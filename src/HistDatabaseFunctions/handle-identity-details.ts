9b27b7eb5dee0df3f9dc4e2697ec61a192d3ba9a
Victordavi <victordavisb@gmail.com>
2024-07-23 19:29:43 -0300

    feat: distance update in applications when user address changes

--- src/HistDatabaseFunctions/handle-identity-details.ts
+++ src/HistDatabaseFunctions/handle-identity-details.ts
@@ -3,6 +3,7 @@ import { historyDatabase, prisma } from '@/lib/prisma';
 import getOpenApplications from './find-open-applications';
 import { findAWSRouteHDB } from './Handle Application/find-AWS-Route';
 import { copyFilesToAnotherFolder } from '@/lib/S3';
+import calculateDistance from '@/utils/Trigger-Functions/search-distance';
 
 /// HDB == HistoryDataBase
 export async function createIdentityDetailsHDB(id: string, candidate_id: string | null, legalResponsibleId: string | null, application_id: string) {
@@ -19,9 +20,9 @@ export async function createIdentityDetailsHDB(id: string, candidate_id: string 
 
     const newId = await historyDatabase.idMapping.findFirst({
         where: { mainId: (mainCandidateId || responsible_id)!, application_id }
-    
+
     })
-   const idField = mainCandidateId ? { candidate_id: newId?.newId } : { responsible_id: newId?.newId };
+    const idField = mainCandidateId ? { candidate_id: newId?.newId } : { responsible_id: newId?.newId };
 
     const createIdentityDetails = await historyDatabase.identityDetails.create({
         data: { main_id: identityId, ...identityDetails, ...idField, application_id }
@@ -31,7 +32,7 @@ export async function createIdentityDetailsHDB(id: string, candidate_id: string 
         return null;
     }
     const route = `CandidateDocuments/${idRoute}/identity/${idRoute}/`;
-    const RouteHDB = await findAWSRouteHDB(idRoute , 'identity', idRoute, null, application_id)
+    const RouteHDB = await findAWSRouteHDB(idRoute, 'identity', idRoute, null, application_id)
     await copyFilesToAnotherFolder(route, RouteHDB)
 
 }
@@ -61,12 +62,56 @@ export async function updateIdentityDetailsHDB(id: string) {
     if (!openApplications) {
         return null;
     }
-
+    const candidateLocation = `${findIdentityDetails.address}, ${findIdentityDetails.addressNumber}, ${findIdentityDetails.neighborhood}, ${findIdentityDetails.city}, ${findIdentityDetails.UF}, ${findIdentityDetails.CEP}`
     for (const application of openApplications) {
-        const updateIdentityDetails = await historyDatabase.identityDetails.updateMany({
-            where: { main_id: identityId, application_id: application.id },
-            data: { ...identityDetails },
-        });
+        const identityDetailsHDB = await historyDatabase.identityDetails.findUnique({
+            where: { application_id: application.id }
+        })
+        if (!identityDetailsHDB) {
+            return null;
+        }
+
+        
+        const candidateApplicationLocation = `${identityDetailsHDB.address}, ${identityDetailsHDB.addressNumber}, ${identityDetailsHDB.neighborhood}, ${identityDetailsHDB.city}, ${identityDetailsHDB.UF}, ${identityDetailsHDB.CEP}`
+        // Verifica se teve alguma alteração no endereço
+        if (candidateApplicationLocation != candidateLocation) {
+            await prisma.$transaction(async (tsPrisma) => {
+                
+                // Primeiro atualizar a tabela no banco de dados de histórico
+                const updateIdentityDetails = await historyDatabase.identityDetails.update({
+                    where: { main_id: identityId, application_id: application.id },
+                    data: { ...identityDetails },
+                });
+
+                // Pegar informações sobre a vaga
+                const educationLevel = await tsPrisma.educationLevel.findUnique({
+                    where: { id: application.educationLevel_id },
+                    include: { announcement: true }
+                });
+                if (!educationLevel) {
+                    return null;
+                }
+                // Descobrir qual a matriz ou filial que a vaga se refere
+                const entityInfo = educationLevel.entitySubsidiaryId ? await tsPrisma.entitySubsidiary.findUnique({
+                    where: { id: educationLevel.entitySubsidiaryId }
+                }) : await tsPrisma.entity.findUnique({
+                    where: { id: educationLevel.announcement.entity_id }
+                });
+
+                if (!entityInfo) {
+                    return null;
+                }
+                
+                const entityLocation = entityInfo.address ? `${entityInfo.address}, ${entityInfo.addressNumber}, ${entityInfo.neighborhood}, ${entityInfo.city}, ${entityInfo.UF}, ${entityInfo.CEP}` : '';
+                // calcular a distância
+                const distance = await calculateDistance(candidateLocation, entityLocation);
+
+                await tsPrisma.application.update({
+                    where: { id: application.id },
+                    data: { distance }
+                });
+            });
+        }
 
     }
 
