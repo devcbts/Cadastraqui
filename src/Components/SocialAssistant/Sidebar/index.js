9685d731ff6f989b6fc55cf840728d71990982f4
devcbts <devcbts01@gmail.com>
2024-07-25 13:49:35 -0300

    Revert "feat: Add assistant schedule skeleton pages"
    This reverts commit 65dba87bd1969dc6be12eec6d25cf785d5f1cd3d.

--- src/Components/SocialAssistant/Sidebar/index.js
+++ src/Components/SocialAssistant/Sidebar/index.js
@@ -1,13 +1,11 @@
 import { ReactComponent as Home } from "Assets/icons/home.svg";
 import { ReactComponent as Profile } from "Assets/icons/profile.svg";
-import { ReactComponent as Schedule } from "Assets/icons/list.svg";
 import Sidebar from "Components/Sidebar";
 export default function SocialAssistantSidebar() {
     return (
         <Sidebar.Root>
             <Sidebar.Item icon={Home} text={'processo seleção'} path={'/home'}></Sidebar.Item>
             <Sidebar.Item icon={Profile} text={'perfil'} path={'/profile'}></Sidebar.Item>
-            <Sidebar.Item icon={Schedule} text={'agenda'} path={'/agenda'}></Sidebar.Item>
         </Sidebar.Root>
     )
 }
