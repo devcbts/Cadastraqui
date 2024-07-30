0cd2d68e131be58e3758782f8d41f5b568c75653
Victordavi <victordavisb@gmail.com>
2024-07-30 13:53:55 -0300

    fix: removed candidate_id parameter for get-candidate-interview route

--- src/http/controllers/candidates/Interview Routes/get-candidate-interviews.ts
+++ src/http/controllers/candidates/Interview Routes/get-candidate-interviews.ts
@@ -7,20 +7,16 @@ import { z } from "zod";
 
 export default async function getCandidateInterviews(request: FastifyRequest, reply: FastifyReply) {
     const params = z.object({
-        candidate_id: z.string(),
         interview_id: z.string().optional()
     })
-    const { candidate_id, interview_id } = params.parse(request.params)
+    const { interview_id } = params.parse(request.params)
     try {
         const user_id = request.user.sub
         const candidateOrResponsible = await SelectCandidateResponsible(user_id)
         if (!candidateOrResponsible) {
             throw new ForbiddenError()
         }
-        const candidateID = candidateOrResponsible.IsResponsible ? candidate_id : candidateOrResponsible.UserData.id
-        if (!candidateID) {
-            throw new ResourceNotFoundError()
-        }
+
 
         if (interview_id) {
             const interview = await prisma.interviewSchedule.findUnique({
@@ -30,7 +26,7 @@ export default async function getCandidateInterviews(request: FastifyRequest, re
             if (!interview) {
                 throw new Error("Entrevista não encontrada")
             }
-            if (!interview.application || interview.application.candidate_id !== candidateID) {
+            if (!interview.application || (interview.application.candidate_id !== candidateOrResponsible.UserData.id || interview.application.responsible_id !== candidateOrResponsible.UserData.id)) {
                 throw new Error("Candidato não vinculado a essa entrevista")
 
             }
@@ -48,29 +44,33 @@ export default async function getCandidateInterviews(request: FastifyRequest, re
 
             return reply.status(200).send({ interview: interviewInfo })
         }
+        const idField = candidateOrResponsible.IsResponsible ? { legalResponsibleId: candidateOrResponsible.UserData.id } : { candidate_id: candidateOrResponsible.UserData.id }
         const applications = await prisma.application.findMany({
-            where: { candidate_id: candidateID },
+            where: idField,
             include: { InterviewSchedule: true },
 
         })
 
 
         // Encontrar entrevistas
-        const interviews = applications
-            .map(application => application.InterviewSchedule);
+        const interviews = applications.map(application => ({
+            interviewSchedule: application.InterviewSchedule,
+            candidateName: application.candidateName
+        }));
 
         const scheduleGrouped: any = {};
 
         const interviewFormatted = interviews.map(interview => {
-            const interviewsApplication = interview
-                .filter(interview => interview.date.getTime() > new Date().getTime() - 30 * 24 * 3600 * 1000)
-                .map(interview => {
+            const interviewsApplication = interview.interviewSchedule
+                .filter(schedule => schedule.date.getTime() > new Date().getTime() - 30 * 24 * 3600 * 1000)
+                .map(schedule => {
                     return {
-                        id: interview.id,
-                        date: interview.date,
-                        hour: `${interview.date.getHours()} : ${interview.date.getMinutes().toString().padStart(2, '0')}`,
-                        interviewType: interview.interviewType === "Interview" ? "Entrevista" : "Visita Domiciliar",
-                        status: interview.InterviewRealized ? "Realizada" : interview.accepted ? "Aceito" : "Recusado",
+                        id: schedule.id,
+                        date: schedule.date,
+                        hour: `${schedule.date.getHours()} : ${schedule.date.getMinutes().toString().padStart(2, '0')}`,
+                        interviewType: schedule.interviewType === "Interview" ? "Entrevista" : "Visita Domiciliar",
+                        status: schedule.InterviewRealized ? "Realizada" : schedule.accepted ? "Aceito" : "Recusado",
+                        candidateName: interview.candidateName // Inclui o nome do candidato
                     };
                 });
 
