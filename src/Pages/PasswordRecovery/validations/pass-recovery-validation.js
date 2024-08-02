d3bac3ba5ed8cbca933e678d81e80bbe9dd07350
devcbts <devcbts01@gmail.com>
2024-08-02 19:05:09 -0300

    fix: Removed old files

--- src/Pages/PasswordRecovery/validations/pass-recovery-validation.js
+++
@@ -1,8 +0,0 @@
-import ValidationBuilder from "../../../validation/builders/validation-builder";
-import ValidationComposite from "../../../validation/composites/validation-composite";
-
-const passwordRecoveryValidation = new ValidationComposite([
-    ...ValidationBuilder.create("password").min(6).required().build()
-])
-
-export default passwordRecoveryValidation
