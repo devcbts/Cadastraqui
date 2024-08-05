014d7492039915f5d569823c71a9263e21b12b06
devcbts <devcbts01@gmail.com>
2024-08-05 19:36:33 -0300

    fix: Fix plugsign webhook route

--- src/http/controllers/signature/routes.ts
+++ src/http/controllers/signature/routes.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from "fastify";
+import fastifyCors from "fastify-cors";
 import updateSign from "./update-sign";
 
 export async function signatureRoutes(app: FastifyInstance) {
+    // for this specific routing, we need to allow every endpoint connection (webhook use)
+    app.register(fastifyCors, {
+        origin: '*',
+        methods: ['POST']
+    });
     app.post('/update-sign', updateSign)
 }
