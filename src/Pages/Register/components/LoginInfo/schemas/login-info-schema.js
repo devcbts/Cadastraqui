e26f84590145554f4b15309a36d212baf71341d0
devcbts <devcbts01@gmail.com>
2024-08-13 14:47:43 -0300

    fix: Add password confirmation on register

--- src/Pages/Register/components/LoginInfo/schemas/login-info-schema.js
+++ src/Pages/Register/components/LoginInfo/schemas/login-info-schema.js
@@ -2,7 +2,15 @@ const { z } = require("zod");
 
 const loginInfoSchema = z.object({
     email: z.string().email('Email inválido').min(1, 'Email obrigatório'),
-    password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres')
+    password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
+    passwordConfirmation: z.string().nullish()
+}).superRefine((data, ctx) => {
+    if (data.password && data.password !== data.passwordConfirmation) {
+        ctx.addIssue({
+            message: 'Senhas não conferem',
+            path: ['passwordConfirmation']
+        })
+    }
 })
 
 export default loginInfoSchema
--- src/utils/enums/document-type.js
+++ src/utils/enums/document-type.js
@@ -2,7 +2,7 @@ const DOCUMENT_TYPE = [
     { value: "DriversLicense", label: "Carteira de Motorista" },
     { value: "FunctionalCard", label: "Carteira Funcional" },
     { value: "MilitaryID", label: "Identidade Militar" },
-    { value: "ForeignerRegistration", label: "Registro de Estrangeiro" },
+    // { value: "ForeignerRegistration", label: "Registro de Estrangeiro" },
     { value: "Passport", label: "Passaporte" },
     { value: "WorkCard", label: "Carteira de Trabalho" },
 ];
