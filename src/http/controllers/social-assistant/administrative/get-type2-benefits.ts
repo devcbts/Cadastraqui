05ac8f1b0c29efb80d64491b2ce3b31dbf2e831a
devcbts <devcbts01@gmail.com>
2024-08-29 15:43:19 -0300

    fix: Fix on type2benefits routes

--- src/http/controllers/social-assistant/administrative/get-type2-benefits.ts
+++ src/http/controllers/social-assistant/administrative/get-type2-benefits.ts
@@ -11,26 +11,33 @@ export default async function getType2Benefits(
 ) {
     const requestParamsSchema = z.object({
         scholarship_id: z.string(),
-        application_id: z.string()
     })
 
-    const { scholarship_id, application_id } = requestParamsSchema.parse(request.params)
+    const { scholarship_id } = requestParamsSchema.parse(request.params)
     try {
 
+        const scholarshipGranted = await prisma.scholarshipGranted.findUnique({
+            where: { id: scholarship_id },
+            include: { application: { select: { id: true } }, type2Benefits: true }
+        })
+        if (!scholarshipGranted?.application_id) {
+            throw new ResourceNotFoundError()
+        }
+        const { application_id } = scholarshipGranted
         const candidateOrResponsible = await SelectCandidateResponsibleHDB(application_id)
         if (!candidateOrResponsible) {
             throw new ForbiddenError()
-            
+
         }
-        const type2Benefits = await prisma.type2Benefit.findMany({
-            where: {
-                scholarshipGrantedId: scholarship_id
-            },
-        })
+        // const type2Benefits = await prisma.type2Benefit.findMany({
+        //     where: {
+        //         scholarshipGrantedId: scholarship_id
+        //     },
+        // })
 
 
         const members = await historyDatabase.familyMember.findMany({
-            where: {application_id}
+            where: { application_id }
         })
 
         const candidate = await historyDatabase.candidate.findUnique({
@@ -53,7 +60,7 @@ export default async function getType2Benefits(
         })
 
         const identityDetails = await historyDatabase.identityDetails.findUnique({
-            where: {application_id}
+            where: { application_id }
         })
         if (!identityDetails) {
             throw new ResourceNotFoundError()
@@ -62,12 +69,15 @@ export default async function getType2Benefits(
         const cadastrante = {
             id: identityDetails.id,
             name: identityDetails.fullName,
-            CPF: identityDetails.CPF,
+            CPF: identityDetails.CPF!,
             profession: identityDetails.profession,
+            // ignore relationship in this case, only set to "null!" to pass ts validation
+            relationship: null!,
             isCandidate: identityDetails.CPF === candidate.CPF
         }
-           
-        return reply.status(200).send({ type2Benefits, cadastrante, formatedMembers })
+        formatedMembers.push(cadastrante)
+
+        return reply.status(200).send({ type2Benefits: scholarshipGranted, cadastrante, formatedMembers })
     } catch (error) {
 
         return reply.status(500).send({ message: 'Internal server error', error })
