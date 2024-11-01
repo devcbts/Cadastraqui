698584cb2856fbbda7a1eca6244519c62fb92c8a
devcbts <devcbts01@gmail.com>
2024-11-01 17:27:40 -0300

    chore: Add string to color lib

--- src/utils/number-to-color.js
+++ src/utils/number-to-color.js
@@ -1,23 +1,20 @@
+import stc from "string-to-color";
+
 export default function toColor(value) {
-    if (isNaN(value)) {
-        const stringHexNumber = (
-            parseInt(
-                parseInt(value, 36)
-                    .toExponential()
-                    .slice(2, -5)
-                , 10) & 0xFFFFFF
-        ).toString(16).toUpperCase().padStart(6, '0F');
-        console.log(value, stringHexNumber)
-        return `#${stringHexNumber}`
-    }
-    let num = value
-    num >>>= 0;
-    // scale factor to prevent low number become all black coloured
-    num *= 100000
-    var b = num & 0xFF,
-        g = (num & 0xFF00) >>> 8,
-        r = (num & 0xFF0000) >>> 16;
-    // a = ((num & 0xFF000000) >>> 24) / 255;
-    // 
-    return "rgb(" + [r, g, b].join(",") + ")";
+    return stc(value)
+    // if (isNaN(value)) {
+    //     const stringHexNumber = stc(value)
+    //     console.log(value, stringHexNumber)
+    //     return stringHexNumber
+    // }
+    // let num = value
+    // num >>>= 0;
+    // // scale factor to prevent low number become all black coloured
+    // num *= 100000
+    // var b = num & 0xFF,
+    //     g = (num & 0xFF00) >>> 8,
+    //     r = (num & 0xFF0000) >>> 16;
+    // // a = ((num & 0xFF000000) >>> 24) / 255;
+    // // 
+    // return "rgb(" + [r, g, b].join(",") + ")";
 }
