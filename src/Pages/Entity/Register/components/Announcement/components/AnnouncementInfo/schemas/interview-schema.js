6de30bde2d0bb8cfcb82035b6f0a05cf724b9456
devcbts <devcbts01@gmail.com>
2025-01-24 16:11:45 -0300

    fix: Fix interview component

--- src/Pages/Entity/Register/components/Announcement/components/AnnouncementInfo/schemas/interview-schema.js
+++ src/Pages/Entity/Register/components/Announcement/components/AnnouncementInfo/schemas/interview-schema.js
@@ -4,8 +4,8 @@ const interviewSchema = z.object({
     startDate: z.string().date('Data inválida'),
     endDate: z.string().date('Data inválida'),
     duration: z.number({ invalid_type_error: 'Duração obrigatória' }),
-    beginHour: z.string().min('Horário de início obrigatório'),
-    endHour: z.string().min('Horário de término obrigatório'),
+    beginHour: z.string().min(1, 'Horário de início obrigatório'),
+    endHour: z.string().min(1, 'Horário de término obrigatório'),
     interval: z.number({ invalid_type_error: 'Intervalo obrigatório' })
 }).superRefine((data, ctx) => {
     if (data.endDate < data.startDate) {
