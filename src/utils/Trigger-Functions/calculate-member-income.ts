2746a2c031fc5f21af4e599c72e5870f13518aa2
Victordavi <victordavisb@gmail.com>
2025-08-31 16:24:21 -0300

    hfix

--- src/utils/Trigger-Functions/calculate-member-income.ts
+++ src/utils/Trigger-Functions/calculate-member-income.ts
@@ -17,7 +17,8 @@ export async function CalculateMemberAverageIncome(member_id: string, source: In
     const totalIncome = memberIncomes.reduce((acc: number, income: any) => {
         return acc + income.liquidAmount
     }, 0)
-    const averageIncome = '0'
+    console.log(totalIncome)
+    const averageIncome = memberIncomes.length > 0 ? totalIncome / memberIncomes.length : '0'
    
     const updateMember = await prisma.familyMemberIncome.updateMany({
         where: { OR: [
