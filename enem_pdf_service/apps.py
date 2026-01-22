57c71f9eab273a2490e25f814bebaba3bce38e64
Victordavi <victordavisb@gmail.com>
2026-01-22 00:58:20 -0300

    feat: initialize ENEM PDF Service with Django app configuration and necessary files

---
+++ enem_pdf_service/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EnemPdfServiceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "enem_pdf_service"
+    verbose_name = "ENEM PDF Service"
